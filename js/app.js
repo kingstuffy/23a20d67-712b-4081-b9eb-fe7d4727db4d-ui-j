48c90634384f4341970dde9b0766fdebc289bb76
Olawale Malomo <warlay234@gmail.com>
2021-09-19 04:33:50 +0100

    Complete rating submission

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const BASE_API_URL = 'http://localhost:1500';
-const DEFAULT_PRODUCT_API_URL = `${ BASE_API_URL }/product/default`;
+const DEFAULT_PRODUCT_SLUG = 'default';
+const DEFAULT_PRODUCT_API_URL = `${ BASE_API_URL }/product/${ DEFAULT_PRODUCT_SLUG }`;
 const REVIEW_API_URL = `${ BASE_API_URL }/review`;
 
 (function (exports) {
@@ -16,11 +17,17 @@ const REVIEW_API_URL = `${ BASE_API_URL }/review`;
     const reviewModal = $('.modal');
     const activeStars = $('.stars--active');
 
+    const form = $('#form');
+    const ratingInput = $('#form__rating');
+    const textInput = $('#form__text');
+    const formError = form.find('.form__error');
+
     const starsTemplate = $('#stars-template').html();
     const reviewItemTemplate = $('#review-item-template').html();
 
     openReviewBtn.click(openReviewModal)
     closeReviewBtn.click(closeReviewModal)
+    form.submit(submitForm)
 
     function openReviewModal() {
       reviewModal.show()
@@ -28,6 +35,40 @@ const REVIEW_API_URL = `${ BASE_API_URL }/review`;
 
     function closeReviewModal() {
       reviewModal.hide();
+      ratingInput.val(0);
+      textInput.val('');
+
+      ratingInput.change();
+    }
+
+    function submitForm(e) {
+      e.preventDefault();
+
+      const rating = Number(ratingInput.val());
+      const text = textInput.val() || '';
+
+      if (rating === 0) {
+        formError.show();
+        return;
+      }
+
+      const body = JSON.stringify({
+        rating,
+        text,
+        product: DEFAULT_PRODUCT_SLUG,
+      })
+
+      fetch(REVIEW_API_URL, {
+        method: 'POST',
+        body
+      })
+        .then(response => response.json())
+        .then(({ data }) => {
+          const { review, averageRating } = data;
+          updateProductRating(averageRating);
+          addProductReview(review, true);
+          closeReviewModal();
+        })
     }
 
     function populateProductDetails(product) {
@@ -38,17 +79,28 @@ const REVIEW_API_URL = `${ BASE_API_URL }/review`;
       productStarsEl.append(starsTemplate);
 
       product.reviews.forEach((review) => {
-        const template = $('<div></div>');
-        template.html(reviewItemTemplate);
-
-        template.find('.reviews__rating-num').text(review.rating);
-        template.find('.reviews__rating-text').text(review.text);
-
-        const reviewStarsTemplate = getStarsTemplate(review.rating);
-        template.find('.reviews__stars').append(reviewStarsTemplate);
-
-        reviewsListEl.append(template);
+        addProductReview(review);
       })
+    }
+
+    function updateProductRating(averageRating) {
+      avgRatingEl.text(averageRating);
+      const starsTemplate = getStarsTemplate(averageRating);
+      productStarsEl.empty();
+      productStarsEl.append(starsTemplate);
+    }
+
+    function addProductReview(review, prepend) {
+      const template = $('<div></div>');
+      template.html(reviewItemTemplate);
+
+      template.find('.reviews__rating-num').text(review.rating);
+      template.find('.reviews__rating-text').text(review.text);
+
+      const reviewStarsTemplate = getStarsTemplate(review.rating);
+      template.find('.reviews__stars').append(reviewStarsTemplate);
+
+      prepend ? reviewsListEl.prepend(template) : reviewsListEl.append(template);
     }
 
     function getStarsTemplate(rating) {
@@ -64,7 +116,7 @@ const REVIEW_API_URL = `${ BASE_API_URL }/review`;
       activeStars.each(function () {
         const stars = $(this);
         const starsOnEl = stars.find('.stars__on');
-        const ratingInput = stars.find('.form__rating');
+        const ratingInput = stars.find('#form__rating');
 
         function updateRating(rating) {
           const width = Number(rating) / 5 * 100;
@@ -83,10 +135,21 @@ const REVIEW_API_URL = `${ BASE_API_URL }/review`;
           })
           .click(function () {
             starsValue = $(this).data('rating');
+            formError.hide();
             updateRating(starsValue);
             ratingInput.val(starsValue);
           })
           .end();
+
+        ratingInput.change(function () {
+          const value = Number($(this).val());
+
+          // Reset values if rating is reset from outside this closure
+          if (value === 0) {
+            starsValue = 0;
+            updateRating(starsValue);
+          }
+        });
       });
     }
 
